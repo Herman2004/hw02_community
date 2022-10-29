4fd15117fa980bb343d999248dc0aaa787f4079a
= <=>
2022-10-29 21:13:53 +0900

    made slug UNIQUE

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -6,7 +7,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(null=True)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
