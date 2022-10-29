05d578e287fd544bf232969537f06e7c58b10e0a
= <=>
2022-10-29 20:05:00 +0900

    added last_posts_quantity variable to make code more readable

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Group, Post
 
 
+last_posts_quantity = 10
+
+
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:last_posts_quantity]
     context = {
         'posts': posts,
     }
@@ -14,7 +17,9 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list'
     group = get_object_or_404(Group, slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by(
+        '-pub_date'
+    )[:last_posts_quantity]
     context = {
         'group': group,
         'posts': posts
