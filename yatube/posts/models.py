5f5db06f0ca296d5642b438e1959da754902346b
= <=>
2022-10-30 12:29:01 +0900

    Added verbose_name to models & changed language of project to Russian

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,21 +5,33 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(null=True)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Название группы"
+    )
+    slug = models.SlugField(
+        null=True,
+        unique=True,
+        verbose_name="Адрес"
+    )
+    description = models.TextField(
+        verbose_name="Описание"
+    )
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(
+        verbose_name="Текст"
+    )
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name="Автор",
     )
     group = models.ForeignKey(
         Group,
