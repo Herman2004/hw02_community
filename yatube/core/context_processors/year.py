14e787b4428f6e913a0b11b67ffcc21de21fcb4c
= <=>
2022-11-18 18:19:27 +0900

    Before final project

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -1,8 +1,12 @@
 import datetime
 
-
+my_age = (datetime.date.today() - datetime.date(
+    2004, 9, 13
+    )) / datetime.timedelta(days=365.45)
+my_age = int(my_age)
 def year(request):
-    """Добавляет переменную с текущим годом."""
+    """Оперирует с годами/датами"""
     return {
-        'current_year_footer': datetime.date.today().year
+        'current_year_footer': datetime.date.today().year,
+        "author_age": my_age
     }
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from posts.models import Group, Post
+from posts.models import Group, Post, User
 
 
 POSTS_QUANTITY_IN_PAGINATOR = 10
@@ -26,3 +26,29 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    # Здесь код запроса к модели и создание словаря контекста
+    the_user = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=the_user)
+    paginator = Paginator(post_list, POSTS_QUANTITY_IN_PAGINATOR)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    posts_quantity = Post.objects.filter(author=the_user).count()
+    context = {
+        'page_obj': page_obj,
+        'User': the_user,
+        'posts_quantity': posts_quantity
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    posts_quantity = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'posts_quantity': posts_quantity
+    }
+    return render(request, 'posts/post_detail.html', context)
